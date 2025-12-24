bfd97ffa51317f180216176541e2daee9d940bf7
Ravindu Pathirana <ravindup.23@cse.mrt.ac.lk>
2025-12-24 23:18:56 +0530

    coursework 02

---
+++ Coursework - 03/Example.java
@@ -0,0 +1,170 @@
+import java.util.*;
+class Example{
+		public static void main(String args[]){
+		Scanner input=new Scanner(System.in);
+	 	String a,b,c,d,e,f,g,h,i,j,k,l,m,n;
+			
+System.out.println("======================================================================================================================");
+System.out.println("||                                                                                                                  ||");
+System.out.println("||                                                                                                                  ||");
+System.out.println("||                                                                                                                  ||");
+System.out.println("||                                                                                                                  ||");
+System.out.println("||                                                @@@@@@@@@@@@@@                                                    ||");
+System.out.println("||                                           @@@@@@@@@@@@@@@@@@@@@@@@                                               ||");
+System.out.println("||                                          @@@@@@@@@@@@@@@@@@@@@@@@@@                                              ||");
+System.out.println("||                                         @@@@         @@         @@@@                                             ||");
+System.out.println("||                                         @@@          @@          @@@                                             ||");
+System.out.println("||                                         @@@          @@          @@@                                             ||");
+System.out.println("||                                         @@@@@@@@@@@@@@@@@@@@@@@@@@@@                                             ||");
+System.out.println("||                                         @@@@@@@@@@@@@@@@@@@@@@@@@@@@                                             ||");
+System.out.println("||                                         @@@@@@@@@@@@@@@@@@@@@@@@@@@@                                             ||");
+System.out.println("||                                         @@@@  @@@@@@@@@@@@@@@@  @@@@                                             ||");
+System.out.println("||                                         @@@    @@@@@@@@@@@@@@    @@@                                             ||");
+System.out.println("||                                         @@@@@@@@@@@@@@@@@@@@@@@@@@@@                                             ||");
+System.out.println("||                                          @@@@@@@@@@@@@@@@@@@@@@@@@@                                              ||");
+System.out.println("||                                                                                                                  ||");
+System.out.println("||                                            @@@@              @@@@                                                ||");
+System.out.println("||                                         @@@@@@@@@@@@@@@@@@@@@@@@@@@@                                             ||");
+System.out.println("||                                       @@@@                        @@@@                                           ||");
+System.out.println("||                                                                                                                  ||");
+System.out.println("||                                                                                                                  ||");
+System.out.println("||    __          __   __                                  __                                                       ||");
+System.out.println("||    \\ \\        / /   | |                                | |                                                       ||");
+System.out.println("||     \\ \\  /\\  / /____| | ____ ____  _ __ ___   ____     | |_ ____                                                 ||");                         
+System.out.println("||      \\ \\/  \\/ /  __ \\ |/  __/  _ \\| '_ ' _ \\ / __ \\    | __/  _ \\                                                ||");              
+System.out.println("||       \\  /\\  /  ____/ |  (_|  (_) | | | | | |  ___/    | ||  (_) |                                               ||");              
+System.out.println("||        \\/  \\/ \\_____| |\\____\\____/| | |_| |_|\\____|     \\__\\____/                                                ||");                      
+System.out.println("||          ______        _ _                            _______ _         _   _                                    ||");                 
+System.out.println("||         |  __  \\      (_) |                          /  _____| |       | | (_)                                   ||");
+System.out.println("||         | |__)  |___ _ _| |_        _____ _ _   _    | (____ | |_ ___ _| |_ _  _____  _ ___                      ||");
+System.out.println("||         |  __  // __' | | \\ \\  /\\  / /   ' | | | |    \\ ___ \\| __/ _ ' | __| |/  _  \\| '__ \\                     ||");
+System.out.println("||         | |  \\ \\ (__| | | |\\ \\/  \\/ / (_)  | |_| |    _____) | || (_|  | |_| |  (_)  | |  | |                    ||");
+System.out.println("||         |_|   \\_\\___,_| | | \\__/\\__/ \\___,_|\\__, |   |______/ \\__\\___,_|\\__|_|\\_____/|_|  | |                    ||");
+System.out.println("||                                              __/ |                                                               ||");
+System.out.println("||                                             |___/                                                                ||");
+System.out.println("||                                                                                                                  ||");
+System.out.println("||                                                                                                                  ||");
+System.out.println("======================================================================================================================");                                                                
+
+System.out.print("Enter Date : " );
+a=input.nextLine();
+System.out.print("Enter Day  : " );
+b=input.nextLine();
+
+System.out.println("                                    ____ __        _   _                                    ");                   
+System.out.println("                                   /  __| |_ ___ _| |_(_)____ __ __                         ");
+System.out.println("                                   \\__  \\  _/ __' |  _| / __ \\  '  \\                        ");
+System.out.println("                                   |____/\\__\\___,_|\\__|_\\____/__||_|                        ");
+System.out.println("                                                                                            ");
+System.out.println("                                 =====================================                      "); 
+
+System.out.print("From : " );
+c=input.nextLine();
+System.out.print("To   : " );
+d=input.nextLine();
+
+System.out.println("                                    _______          _                                             ");
+System.out.println("                                   |__   __| _  __ _(_)_ _  ____                                   ");
+System.out.println("                                      | | | '_/ __' | | ' \\(_-<                                    ");
+System.out.println("                                      |_| |_| \\___,_|_|_||_/__/                                    ");
+System.out.println("                                                                                                   ");
+System.out.println("                                   ==============================                                  "); 
+System.out.println("                                                                                                     "); 
+System.out.println("                                                                                                     "); 
+
+System.out.println("    _____                    __              ___             _                                       "); 
+System.out.println("   /  __/___ ___ _  __ _____/ / _____ __    / _ \\_____ _  __(_)                                      "); 
+System.out.println("  _\\  \\/ __ '/  ' \\/ // / _  / ___/ _' /   / // /  - _) |/ / /                                       "); 
+System.out.println(" /____/\\__,_/_/_/_/\\_,_/\\_,_/_/   \\_,_/   /____/\\____/|___/_/                                        "); 
+System.out.println("                                                      												 "); 
+System.out.println("                                                                                                     "); 
+
+System.out.print("Arrival-Time  : ");
+e=input.nextLine();
+System.out.print("Depature-Time : ");
+f=input.nextLine();
+
+System.out.println("                                                                   ");
+System.out.println("    _____                         _ __                             ");
+System.out.println("   /  __/___ _____  ______ ______(_) / ____ _                      ");    
+System.out.println("  _\\  \\/ __' / __ ' /  __ '/  __/ /  '_/ _ '/                      ");
+System.out.println(" /____/\\__,_/\\___, /\\__,__/__/ /_/_/\\_\\__,_/                       ");
+System.out.println("            /_____/                                                ");
+System.out.println("                                                                   ");
+
+System.out.print("Arrival-Time  : ");
+g=input.nextLine();
+System.out.print("Depature-Time : ");
+h=input.nextLine();
+
+System.out.println("                                                                    ");
+System.out.println("    ___       __                     __ __                     _    ");
+System.out.println("   / _ \\__ __/ /  __ _____  __ __   / //_/__ ____ _  ___ _____(_)   ");
+System.out.println("  / , _/ // / _ \\/ // / _ \\/ // /  / ,< /  // /  ' \\/ _ '/ __/ /    ");
+System.out.println(" /_/|_|\\_,_/_//_/\\_,_/_//_/\\_,_/  /_/|_|\\__,_/_/_/_/\\_,_/_/ /_/    ");
+System.out.println("                                                                   ");
+
+System.out.print("Arrival-Time  : ");
+i=input.nextLine();
+System.out.print("Depature-Time : ");
+j=input.nextLine();
+
+System.out.println("                                                                    ");
+System.out.println("  ______      __        __ __                     _    ");
+System.out.println(" /  ___/___ _/ /_ __   / //_/__ ____ _  ___ _____(_)   ");
+System.out.println("/  (__/  _ '/ / // /  / ,< /  // /  ' \\/ _ '/ __/ /    ");
+System.out.println("\\____/\\__,_/_/\\_,_/  /_/|_|\\__,_/_/_/_/\\_,_/_/ /_/    ");
+System.out.println("                                                                   ");
+
+System.out.print("Arrival-Time  : ");
+k=input.nextLine();
+System.out.print("Depature-Time : ");
+l=input.nextLine();
+
+System.out.println("                                                                                                           ");
+System.out.println("   ___          _                 __            ___          _ _        _                                  ");
+System.out.println("  / _ \\___ __  (_)___  ________ _/ /______ _   / _ \\____ _  (_|_) ___  (_)                                 ");
+System.out.println(" / , _/ __' / / / __ '/ __/ __ '/  __/ __ '/  / , _/ __' / /  / / _  \\/ /                                  ");
+System.out.println("/_/|_|\\__,_/_/ /\\__,_/_/  \\__,_/\\___/\\__,_/  /_/|_|\\__,_/_/  / /_/ /_/ /                                   ");
+System.out.println("                                                                                                           ");
+
+System.out.print("Arrival-Time  : ");
+m=input.nextLine();
+System.out.print("Depature-Time : ");
+n=input.nextLine();
+
+System.out.println("                                                                                                                      ");
+System.out.println("                                                                                                                      ");  
+System.out.println("===================================================================================================================== ");
+
+
+System.out.println("                                                                                                                   ");                                                                 
+System.out.println("          +------------------------------------------------------------------------------------------------------+");
+System.out.println("          |    ____       _ _                         _____  _         _   _                                     |");
+System.out.println("          |    | _ \\___ _(_) |_  __  _____ _ _  _    /  ___|| |_ ___ _| |_(_)____ __ __                          |");
+System.out.println("          |    |   / __' | | \\ \\/  \\/ / __' | || |   \\___  \\|  _/ __' |  _| / __ \\  '  \\                         |");
+System.out.println("          |    |_|_\\___,_|_|_|\\__/\\__/\\___,_|\\_, |   |_____/\\___\\___,_|\\__| \\____/__||__|                        |");
+System.out.println("          |                                  |__/                                                                |");
+System.out.println("          |                                                                                                      |");
+System.out.println("          |    Day : "+b+"                                                          Date : "+a+"         |");
+System.out.println("          |    From: "+c+"                                                             To   : "+d+"            |");
+System.out.println("          |                                                                                                      |");
+System.out.println("          +-----------------------------+------------------------------------+-----------------------------------+");
+System.out.println("          |           Train             |          Arrival Time              |             Departure Time        |");
+System.out.println("          +-----------------------------+------------------------------------+-----------------------------------+");                        
+System.out.println("          |                             |                                    |                                   |");
+System.out.println("          | Samudra Devi                |       "+e+"                       |           "+f+"                  |"); 
+System.out.println("          |                             |                                    |                                   |");
+System.out.println("          | Sagarika                    |       "+g+"                       |           "+h+"                  |"); 
+System.out.println("	  |                             |                                    |                                   |");
+System.out.println("          | Ruhunu Kumari               |       "+i+"                       |           "+j+"                  |"); 
+System.out.println("          |                             |                                    |                                   |");
+System.out.println("          | Galu Kumari                 |       "+k+"                       |           "+l+"                  |"); 
+System.out.println("          |                             |                                    |                                   |");
+System.out.println("          | Rajarata Rajini             |       "+m+"                      |           "+n+"                 |"); 
+System.out.println("          |                             |                                    |                                   |");
+System.out.println("          +------------------------------------------------------------------------------------------------------+");
+
+	}
+}	
+		
