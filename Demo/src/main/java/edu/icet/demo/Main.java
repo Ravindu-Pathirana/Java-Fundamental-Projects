2e21258f976152295590e8ec502e79307ed35374
Ravindu Pathirana <ravindup.23@cse.mrt.ac.lk>
2026-01-12 09:39:20 +0530

    Creating java project

---
+++ Demo/src/main/java/edu/icet/demo/Main.java
@@ -0,0 +1,11 @@
+package edu.icet.demo;
+
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList list = new ArrayList();
+
+        list.
+    }
+}
