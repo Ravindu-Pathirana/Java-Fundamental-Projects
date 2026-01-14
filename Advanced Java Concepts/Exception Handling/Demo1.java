92bca109d7b2e2fc792a494162fa417930d79f77
Ravindu Pathirana <ravindup.23@cse.mrt.ac.lk>
2026-01-14 23:26:04 +0530

    Exception handling using try catch block

---
+++ Advanced Java Concepts/Exception Handling/Demo1.java
@@ -0,0 +1,8 @@
+public class Demo1 {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        int x,a=12,b=0;
+        x=a/b; //
+        System.out.println("End");
+    }
+}
---
+++ Advanced Java Concepts/Exception Handling/Demo2.java
@@ -0,0 +1,13 @@
+public class Demo2 {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        int x,a=12,b=0;
+        try{
+            x=a/b; //
+        }catch(ArithmeticException ex){
+            x=-1;
+        }
+        System.out.println("x : "+x);
+        System.out.println("End");
+    }
+}
