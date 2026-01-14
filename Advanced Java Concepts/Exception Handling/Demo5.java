25dccc21351fa1bc7a082cddd7d5815608d2bc03
Ravindu Pathirana <ravindup.23@cse.mrt.ac.lk>
2026-01-14 23:59:21 +0530

    Update Demo5.java

--- Advanced Java Concepts/Exception Handling/Demo5.java
+++ Advanced Java Concepts/Exception Handling/Demo5.java
@@ -1,18 +1,17 @@
 class Demo{
-public static void main(String args[]){
-System.out.println("Start");
-int[] ar={10,20,30};
-int x,a=4,b=0;
-try{
-try{
-ar[3]=100;
-}catch(ArithmeticException ex){
-System.out.println("Inner Catch : "+ex.getMessage());
-}
-}catch(RuntimeException ex){
-System.out.println("Outer Catch : "+ex.getMessage());
-}
-System.out.println("End.
-");
-}
+    public static void main(String args[]){
+        System.out.println("Start");
+        int[] ar={10,20,30};
+        int x,a=4,b=0;
+        try{
+            try{
+                ar[3]=100;
+            }catch(ArithmeticException ex){
+                System.out.println("Inner Catch : "+ex.getMessage());
+            }
+        }catch(RuntimeException ex){
+            System.out.println("Outer Catch : "+ex.getMessage());
+        }
+        System.out.println("End.");
+    }
 }
