fac9decddad46648735109604ab1c74c68f485b2
Ravindu Pathirana <ravindup.23@cse.mrt.ac.lk>
2026-01-15 08:53:56 +0530

    If there is many exceptions, use super class for catch all those exceptions

---
+++ Advanced Java Concepts/Exception Handling/Demo6.java
@@ -0,0 +1,14 @@
+class Demo6 {
+    public static void main(String args[]) {
+        System.out.println("Start");
+        int[] ar = {10, 20, 30};
+        String s = "abcd";
+        int x = 5, a = 0;
+        try {
+            ar[x] = s.charAt(4) / a;
+        } catch (RuntimeException ex) {
+            System.out.println(ex.getMessage());
+        }
+        System.out.println("End.");
+    }
+}
