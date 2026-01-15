5e8fd200c5dc0bf2e4c91598897fcd96bcd8c9d2
Ravindu Pathirana <ravindup.23@cse.mrt.ac.lk>
2026-01-15 09:01:52 +0530

    Create Demo8.java
    Super classes are in the most below in the try catch ladder. It means first we must use sub classes according to the class hierarchy.

---
+++ Advanced Java Concepts/Exception Handling/Demo8.java
@@ -0,0 +1,20 @@
+class Demo{
+    public static void main(String args[]){
+        System.out.println("Start");
+        int[] ar={10,20,30};
+        String s= "abcd";
+        int x=5,a=2;
+        try{
+            ar[x]=s.charAt(0)/a;
+        }catch(ArithmeticException ex){
+            System.out.println("Arithmetic : "+ex.getMessage());
+        }catch(ArrayIndexOutOfBoundsException ex){
+            System.out.println("Array Index : "+ex.getMessage());
+        }catch(IndexOutOfBoundsException ex){
+            System.out.println("Any Index : "+ex.getMessage());
+        }catch(StringIndexOutOfBoundsException ex){ //
+            System.out.println("String Index : "+ex.getMessage());
+        }
+        System.out.println("End. ");
+    }
+}
