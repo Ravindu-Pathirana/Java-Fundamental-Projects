7757c9f81cfc85f5930a895e4f6e93e5df02a580
Ravindu Pathirana <ravindup.23@cse.mrt.ac.lk>
2026-01-17 19:40:05 +0530

    Get web request in spring boot backend

---
+++ Advanced Web Development/Demo-project-Day-03/src/main/java/edu/icet/crm/customer/controller/CityController.java
@@ -0,0 +1,20 @@
+package edu.icet.crm.customer.controller;
+
+import edu.icet.crm.customer.controller.service.CityService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController
+
+public class CityController {
+
+    @GetMapping("/city-by-name/{postalCode}")
+    public Map<String, String> getCityNameByPostalCode(@PathVariable String postalCode){
+        System.out.println(postalCode);
+        CityService service = new CityService();
+        return service.getCityByName(postalCode);
+    }
+
+}
---
+++ Advanced Web Development/Demo-project-Day-03/src/main/java/edu/icet/crm/customer/controller/service/CityService.java
@@ -0,0 +1,26 @@
+package edu.icet.crm.customer.controller.service;
+
+import org.springframework.util.StringUtils;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CityService {
+
+    public Map<String,String> getCityByName(String postalcode){
+        HashMap<String,String> cities = new HashMap<>();
+        cities.put("10400","Moratuwa");
+        cities.put("10500","Mount - Lavinia");
+        cities.put("10800","Colpetty");
+
+        String city = cities.get(postalcode);
+
+        if (StringUtils.hasText(city)){
+            return Collections.singletonMap("data",city) ;
+        }
+
+        return Collections.singletonMap("data","No city listed.");
+    }
+
+}
