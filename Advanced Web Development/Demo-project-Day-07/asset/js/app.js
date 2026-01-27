5271db895474b1e675b831d1f1ac8d7390cccb32
Ravindu Pathirana <ravindup.23@cse.mrt.ac.lk>
2026-01-27 17:06:20 +0530

    create app.js

---
+++ Advanced Web Development/Demo-project-Day-07/asset/js/app.js
@@ -0,0 +1,41 @@
+//Event listener for saving a product
+document
+  .getElementById("btn-save-product")
+  .addEventListener("click", saveProduct);
+
+//Function to save product data
+function saveProduct() {
+  let requestBody = mapRequest();
+
+  fetch("http://localhost:8080/product", {
+    method: "POST",
+    body: JSON.stringify(requestBody),
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log("Product saved successfully:", data);
+    })
+    .catch((error) => {
+      console.error("Error saving product:", error);
+    });
+}
+
+//Function to map form data to request body
+function mapRequest() {
+  let requestBody = {
+    name: undefined,
+    description: undefined,
+    price: undefined,
+    category: undefined,
+  };
+
+  requestBody.name = document.getElementById("txt-product-name").value;
+  requestBody.description = document.getElementById(
+    "txt-product-description",
+  ).value;
+  requestBody.price = parseFloat(
+    document.getElementById("txt-product-price").value,
+  );
+  requestBody.category = document.getElementById("txt-product-category").value;
+  return requestBody;
+}
